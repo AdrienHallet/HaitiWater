919b04c77dc7b493cea57b37c84670fe03892878
Unknown <adrien.hallet@student.uclouvain.be>
2018-10-29 22:37:48 +0100

    Dashboard creation

--- code/haitiwater/haitiwater/urls.py
+++ code/haitiwater/haitiwater/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.authentication.urls'))
+    path('', include('apps.dashboard.urls')),  # ToDo change to login page
+    path('accueil', include('apps.dashboard.urls')),
 ]
