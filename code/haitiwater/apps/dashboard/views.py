5d2b454e65ab5f480916fff255757f77e74f2eba
adhallet <adrien.hallet@student.uclouvain.be>
2018-10-30 17:44:01 +0100

    Dashboard template initialisation

--- code/haitiwater/apps/dashboard/views.py
+++ code/haitiwater/apps/dashboard/views.py
@@ -5,7 +5,7 @@ from haitiwater.settings import PROJECT_VERSION, PROJECT_NAME
 
 
 def index(request):
-    template = loader.get_template('base.html')
+    template = loader.get_template('dashboard.html')
     context = {
         'project_version': PROJECT_VERSION,
         'project_name': PROJECT_NAME,
