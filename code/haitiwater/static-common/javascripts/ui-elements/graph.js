0fe71aa735870d3c2ee221969d9569a1dc1b03f2
adhallet <adrien.hallet@student.uclouvain.be>
2018-11-04 23:26:40 +0100

    Callback and listener to change displayed graph

--- code/haitiwater/static-common/javascripts/ui-elements/graph.js
+++ code/haitiwater/static-common/javascripts/ui-elements/graph.js
@@ -1,38 +1,36 @@
-jsonfile = null;
-dataURL = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '') + "/api/graph/?type=json";
+// Call refresh when loading so that the first graph in the list is displayed
+refresh();
 
-//ToDo next iteration, use this one
-$.getJSON(dataURL, function() {
-    console.log("1")
-}).done( function() {
-    console.log("2")
-}).fail( function(e) {
-    console.log(e)
-}).always( function() {
-    console.log("4")
-});
-
-//ToDo move up to allow failure
-$.getJSON(dataURL, function(jsonfile) {
-    var labels = jsonfile.jsonarray.map(function(e) {
-       return e.name;
+// Request the JSON from server and update the graph
+function refresh(){
+    let baseURL = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '');
+    let dataURL = baseURL + "/api/graph/?type=" + document.getElementById("graphTitle").value;
+    console.log(dataURL)
+    $.getJSON(dataURL, function(jsonfile) {
+        var labels = jsonfile.jsonarray.map(function(e) {
+           return e.name;
+        });
+        var data = jsonfile.jsonarray.map(function(e) {
+           return e.age;
+        });
+        var ctx = canvas.getContext('2d');
+        var config = {
+           type: 'line',
+           data: {
+              labels: labels,
+              datasets: [{
+                 label: 'Graph Line',
+                 data: data,
+                 backgroundColor: 'rgba(0, 119, 204, 0.3)'
+              }]
+           }
+        };
+        var chart = new Chart(ctx, config);
+    }).done( function() {
+        console.log("Finished graph request")
+    }).fail( function(e) {
+        console.log("Failed graph request")
+    }).always( function() {
+        console.log("Closing graph request")
     });
-    var data = jsonfile.jsonarray.map(function(e) {
-       return e.age;
-    });
-
-    var ctx = canvas.getContext('2d');
-    var config = {
-       type: 'line',
-       data: {
-          labels: labels,
-          datasets: [{
-             label: 'Graph Line',
-             data: data,
-             backgroundColor: 'rgba(0, 119, 204, 0.3)'
-          }]
-       }
-    };
-
-    var chart = new Chart(ctx, config);
-});
+}
