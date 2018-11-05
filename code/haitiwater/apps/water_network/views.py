8cb609a0bd8517cb8b2d740abc9c507284c4e4f0
adhallet <adrien.hallet@student.uclouvain.be>
2018-11-05 15:48:14 +0100

    Water network - right info widget

--- code/haitiwater/apps/water_network/views.py
+++ code/haitiwater/apps/water_network/views.py
@@ -1,28 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.template import loader
-from django.template.loader import render_to_string
 from django_tables2 import RequestConfig
 from django_tables2.export.export import TableExport
-from chartjs.views.lines import BaseLineChartView
-
 from .classes.water_element_table import DummyTable, DummyFilter
-
 from apps.water_network.models import Dummy
 from haitiwater.settings import PROJECT_VERSION, PROJECT_NAME
-
-def graph(request):
-    export_format = request.GET.get('type', None)
-    if export_format == "json":
-        export_format = """{
-               "jsonarray": [{
-                  "name": "Joe",
-                  "age": 12
-               }, {
-                  "name": "Tom",
-                  "age": 14
-               }]}"""
-    return HttpResponse(export_format)
 
 
 def index(request):
@@ -39,23 +21,8 @@ def index(request):
     context = {
         'project_version': PROJECT_VERSION,
         'project_name': PROJECT_NAME,
-        'network_element': debug_fill_table(),
         'dummy': filter.qs,
-        'graph_json': export_format,
+        'zone_name': "Nom de la zone",  # Todo Backend
         'filter': filter,
     }
     return HttpResponse(template.render(context, request))
-
-def debug_fill_table():
-    table = []
-    for i in range(1000):
-        table.append({
-                'id': 1,
-                'type': 'Fontaine',
-                'address': 'Rue du bois joli, 4',
-                'users': 600,
-                'state': "En service",
-                'volume_m3': 10,
-                'volume_gal': 2641.72,
-            })
-    return table
