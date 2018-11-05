a75b2396a4cda03c47bff6293f91845fa79484f4
Sébastien Strebelle <sebastien.strebelle@student.uclouvain.be>
2018-11-05 18:44:27 +0100

    Three models : Zones, Locations and Element

--- code/haitiwater/apps/water_network/models.py
+++ code/haitiwater/apps/water_network/models.py
@@ -1,6 +1,74 @@
-from django.db import models
+from django.contrib.gis.db import models
+from enum import Enum
 
-# Create your models here.
+
+#########
+# Enums #
+#########
+
+# https://hackernoon.com/using-enum-as-model-field-choice-in-django-92d8b97aaa63
+
+
+class ElementType(Enum):
+    # Entrée d'eau
+    SOURCE = "Source"
+    # Sortie d'eau
+    FOUNTAIN = "Fontaine"
+    KIOSK = "Kiosque"
+    INDIVIDUAL = "Prise individuelle"
+    # Conduite
+    PIPE = "Tuyau"
+
+
+class ElementStatus(Enum):
+    OK = "En service"
+    REPAIR = "En réparation"
+    URGENT = "Hors service (Urgent)"
+    KO = "Hors service"
+
+
+##########
+# Models #
+##########
+
+
+class Zone(models.Model):
+
+    name = models.CharField("Nom", max_length=50)
+    superzone = models.ForeignKey('self', verbose_name="Super-zone", related_name='subzones', on_delete=models.CASCADE)
+    # TODO see if ManyToMany ?
+
+    # Generated : subzones, locations
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+
+    zone = models.ForeignKey(Zone, verbose_name="Zone", related_name="locations", on_delete=models.CASCADE)
+    lon = models.FloatField("Longitude")
+    lat = models.FloatField("Latitude")
+    poly = models.MultiPolygonField("Multi-polygone")
+
+    # Generated : elements
+
+    def __str__(self):
+        return self.zone.name + " : (" + str(self.lon) + ", " + str(self.lat) + ")"
+
+
+class Element(models.Model):
+
+    name = models.CharField("Nom", max_length=50)
+    type = models.CharField("Type", max_length=20, choices=[(i.name, i.value) for i in ElementType])
+    status = models.CharField("État", max_length=20, choices=[(i.name, i.value) for i in ElementStatus])
+    location = models.ForeignKey(Location, verbose_name="Localité", related_name="elements", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+# old
 
 
 class Dummy(models.Model):
