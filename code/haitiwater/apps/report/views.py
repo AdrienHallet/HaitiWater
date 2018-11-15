b2dc95b3a08e2f1d8b46515ddb5bb58644e9a733
adhallet <adrien.hallet@student.uclouvain.be>
2018-11-15 18:56:41 +0100

    Report page skeleton

---
+++ code/haitiwater/apps/report/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.template import loader
+from haitiwater.settings import PROJECT_VERSION, PROJECT_NAME
+
+
+def index(request):
+    template = loader.get_template('report.html')
+    context = {
+        'project_version': PROJECT_VERSION,
+        'project_name': PROJECT_NAME,
+    }
+    return HttpResponse(template.render(context, request))
