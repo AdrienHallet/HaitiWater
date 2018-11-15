d6b20b6937b0f5235c7030c23090a594cce5d49a
adhallet <adrien.hallet@student.uclouvain.be>
2018-11-15 04:30:00 +0100

    Use localstorage to save menu collapsed state

--- code/haitiwater/static-common/javascripts/theme.custom.js
+++ code/haitiwater/static-common/javascripts/theme.custom.js
@@ -1,1 +1,15 @@
-/* Add here all your JS customizations */
+$( document ).ready(function() {
+
+    //Get local storage value or true (as it is default to be open)
+    let isMenuOpen = (localStorage.getItem('isMenuOpen') === 'true');
+    if(!isMenuOpen){
+        $('html').addClass('sidebar-left-collapsed')
+    }
+
+    //Toggle menu position on localstorage to save collapsed state
+    $('.sidebar-toggle').on('click', function (){
+        isMenuOpen = !isMenuOpen;
+        localStorage.setItem('isMenuOpen', isMenuOpen.toString());
+    })
+
+});
