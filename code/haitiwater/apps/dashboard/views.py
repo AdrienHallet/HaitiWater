34638baea954d14c09c21fbaf7e5eb6f165b0804
CelineDknp <celine.deknop@student.uclouvain.be>
2018-12-12 22:37:50 +0100

    Details in dashboard

--- code/haitiwater/apps/dashboard/views.py
+++ code/haitiwater/apps/dashboard/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
+from ..water_network.models import Element
+from ..consumers.models import Consumer
 from django.template.loader import render_to_string
 from haitiwater.settings import PROJECT_VERSION, PROJECT_NAME
 
@@ -10,11 +12,32 @@ def index(request):
         'project_version': PROJECT_VERSION,
         'project_name': PROJECT_NAME,
         'zone_name': 'Nom de la zone',  # Todo backdend
-        'amount_fountain': 42,  # Todo backend
-        'amount_kiosk': 42,  # Todo backend
-        'amount_individual': 42,  # Todo backend
-        'amount_pipe': 42,  # Todo backend
-        'amount_registered_consumers': 20,  # Todo backend
-        'amount_individual_consumers': 40,  # Todo backend
+        'amount_fountain': get_amount_fountain(),
+        'amount_kiosk': get_amount_kiosk(),
+        'amount_individual': get_amount_individual(),
+        'amount_pipe': get_amount_pipe(),
+        'amount_registered_consumers': get_amount_consumer(),
+        'amount_individual_consumers': get_amount_indiv_consummer()
     }
     return HttpResponse(template.render(context, request))
+#TODO zone
+def get_amount_fountain():
+    return len(Element.objects.filter(type="FOUNTAIN"))
+
+def get_amount_kiosk():
+    return len(Element.objects.filter(type="KIOSK"))
+
+def get_amount_individual():
+    return len(Element.objects.filter(type="INDIVIDUAL"))
+
+def get_amount_pipe():
+    return len(Element.objects.filter(type="PIPE"))
+
+def get_amount_consumer():
+    return len(Consumer.objects.all())
+
+def get_amount_indiv_consummer():
+    result = 0
+    for consumer in Consumer.objects.all():
+        result += consumer.household_size
+    return result
