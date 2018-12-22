700362df80165741fa28611386a1b58da642a48e
CelineDknp <celine.deknop@student.uclouvain.be>
2018-12-22 19:59:09 +0100

    refactor of edit done

--- code/haitiwater/apps/api/exports.py
+++ code/haitiwater/apps/api/exports.py
@@ -9,6 +9,7 @@ from ..report.models import Report, Ticket
 from django.contrib.auth.models import User, Group
 from ..api.get_table import *
 from ..api.add_table import *
+from ..api.edit_table import *
 
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
@@ -158,103 +159,6 @@ def edit_element(request):
     else:
         return HttpResponse("Impossible d'éditer la table "+element+
                             ", elle n'est pas reconnue", status=500)
-
-
-def edit_water_element(request):
-    id = request.POST.get("id", None)
-    elems = Element.objects.filter(id=id)
-    if len(elems) < 0:
-        return error_404
-    elem = elems[0]
-    elem.type = request.POST.get("type", None).upper()
-    elem.location = request.POST.get("localization", None)
-    elem.status = request.POST.get("state", None).upper()
-    elem.save()
-    return success_200
-
-
-def edit_consummer(request):
-    id = request.POST.get("id", None)
-    consummers = Consumer.objects.filter(id=id)
-    if len(consummers) < 0:
-        return error_404
-    consummer = consummers[0]
-    consummer.first_name = request.POST.get("firstname", None)
-    consummer.last_name = request.POST.get("lastname", None)
-    consummer.gender = request.POST.get("gender", None)
-    consummer.location = request.POST.get("address", None)
-    consummer.household_size = request.POST.get("subconsumer", None)
-    consummer.phone = request.POST.get("phone", None)
-    outlet_id = request.POST.get("mainOutlet", None)
-    outlet = Element.objects.filter(id=outlet_id)
-    if len(outlet) > 0:
-        outlet = outlet[0]
-    else:
-        return error_404  # Outlet not found, can't create
-    consummer.water_outlet = outlet
-    consummer.save()
-    return success_200
-
-
-def edit_zone(request):
-    id = request.POST.get("id", None)
-    zone = Zone.objects.filter(id=id)
-    if len(zone) < 0:
-        return error_404
-    zone = zone[0]
-    old_name = zone.name
-    zone.name = request.POST.get("name", None)
-    zone.subzones.remove(old_name)
-    zone.subzones.append(zone.name)
-    for z in Zone.objects.all():
-        if old_name in z.subzones:
-            z.subzones.remove(old_name)
-            z.subzones.append(zone.name)
-            z.save()
-    zone.save()
-    return success_200
-
-
-def edit_manager(request):
-    id = request.POST.get("id", None)
-    user = User.objects.filter(username=id)
-    if len(user) == 1:
-        user = user[0]
-        type = request.POST.get("type", None)
-        if type == "fountain-manager":
-            water_out = request.POST.get("outlets", None)
-            if len(water_out) > 1:
-                res = Element.objects.filter(id__in=water_out)
-            else:
-                res = Element.objects.filter(id=water_out)
-            if len(res) > 0:
-                for outlet in res:
-                    user.profile.outlets = []
-                    user.profile.outlets.append(outlet.id)
-            my_group = Group.objects.get(name='Gestionnaire de fontaine')
-            my_group.user_set.add(user)
-            if user.profile.zone: #If user had a zone, switch it
-                g = Group.objects.get(name='Gestionnaire de zone')
-                g.user_set.remove(user)
-            user.save()
-        elif type == "zone-manager":
-            zone = request.POST.get("zone", None)
-            res = Zone.objects.filter(id=zone)
-            if len(res) == 1:
-                user.profile.zone = res[0]
-            else:
-                return HttpResponse("Impossible d'assigner cette zone", status=404)
-            my_group = Group.objects.get(name='Gestionnaire de zone')
-            my_group.user_set.add(user)
-            if len(user.profile.outlets) > 0: #If user had outlets
-                g = Group.objects.get(name='Gestionnaire de fontaine')
-                g.user_set.remove(user)
-                user.profile.outlets = []
-            user.save()
-    else:
-        return HttpResponse("Utilisateur introuvable dans la base de donnée",
-                          status=404)
-    return success_200
 
 
 def parse(request):
