353ce345a25078191731a071d62c043ee8f2bbd2
adhallet <adrien.hallet@student.uclouvain.be>
2019-01-29 12:09:24 +0100

    Collapsible-toggable right drawer for details

--- code/haitiwater/apps/water_network/static/gis.js
+++ code/haitiwater/apps/water_network/static/gis.js
@@ -1,13 +1,14 @@
 let MAP_CENTER = new L.latLng(18.579916, -72.294903); // Port-au-Prince airport
 let gisMap = 'undefined'
 
+// Callback from django-leaflet on map initialization
 function waterGISInit(map, options) {
     gisMap = map
     // Set map center to haiti
     map.setView(MAP_CENTER, 8);
 
     var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            osmAttrib = '',
             osm = L.tileLayer(osmUrl, { maxZoom: 18, attribution: osmAttrib }),
             drawnItems = L.featureGroup().addTo(map);
 
@@ -15,7 +16,7 @@ function waterGISInit(map, options) {
     L.control.layers({
         'OSM': osm.addTo(map),
         "Google": L.tileLayer('http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}', {
-            attribution: 'google'
+            attribution: 'Google'
         })
     }, { 'drawlayer': drawnItems }, { position: 'topleft', collapsed: false }).addTo(map);
     map.addControl(new L.Control.Draw({
@@ -32,6 +33,7 @@ function waterGISInit(map, options) {
             }
         }
     }));
+    map.addControl(new drawerControl());
 
     map.on(L.Draw.Event.CREATED, function (event) {
         var layer = event.layer;
@@ -43,6 +45,25 @@ function waterGISInit(map, options) {
     $(".leaflet-draw-toolbar").attr('hidden', true)
 }
 
+var drawerControl = L.Control.extend({
+    options: {
+        position: 'topright'
+        //control position - allowed: 'topleft', 'topright', 'bottomleft', 'bottomright'
+    },
+
+    onAdd: function (map) {
+        var container = L.DomUtil.create('i', 'leaflet-bar leaflet-control leaflet-drawer-control fas fa-arrow-left');
+        container.style.backgroundColor = 'white';
+        container.style.width = '25px';
+        container.style.height = '25px';
+        container.onclick = function(){
+            toggleDrawer();
+        }
+        return container;
+    }
+
+});
+
 function drawTest(){
     console.log("starting");
     let map = getMap();
@@ -53,4 +74,24 @@ function drawTest(){
 
 function getMap(){
     return gisMap;
+}
+
+// Toggle the "details" drawer
+function toggleDrawer(){
+    let mapContainer = $('#map-container');
+    let controlContainer = $('.leaflet-drawer-control')
+
+    if (mapContainer.hasClass('col-md-9')){
+        mapContainer.removeClass('col-md-9');
+        mapContainer.addClass('col-md-12');
+        controlContainer.removeClass('fa-arrow-right');
+        controlContainer.addClass('fa-arrow-left');
+    }
+    else {
+        mapContainer.addClass('col-md-9');
+        mapContainer.removeClass('col-md-12');
+        controlContainer.addClass('fa-arrow-right');
+        controlContainer.removeClass('fa-arrow-left');
+    }
+    $('#details').collapse('toggle');
 }
