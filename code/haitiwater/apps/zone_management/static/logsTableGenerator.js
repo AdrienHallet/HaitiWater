50b9a5eb5e4be7fe4d8fbdcf923d0970350f0343
Unknown <adrien.hallet@student.uclouvain.be>
2019-02-11 08:57:29 +0100

    Back request to accept/revert

--- code/haitiwater/apps/zone_management/static/logsTableGenerator.js
+++ code/haitiwater/apps/zone_management/static/logsTableGenerator.js
@@ -36,12 +36,12 @@ function drawLogTable(){
     $('#datatable-logs tbody').on( 'click', '.revert-modification', function () {
         let data = $(this).parents('tr')[0].getElementsByTagName('td');
         if (confirm("Voulez-vous annuler: " + data[1].innerText + ' ' + data[2].innerText + ' ?')){
-            revertModification("water_element", data[0].innerText);
+            transactionAction('revert', data[0].innerText);
         } else {}
     } );
     $('#datatable-water_element tbody').on( 'click', '.accept-modification', function () {
         let data = $(this).parents('tr')[0].getElementsByTagName('td');
-        acceptModification(data);
+        transactionAction('accept', data[0].innerText);
     } );
     prettifyHeader('logs');
 }
@@ -95,4 +95,26 @@ function getLogsActionButtonsHTML(){
     return '<div class="center"><a style="cursor:pointer;" class="accept-modification fas fa-check-square"></a>' +
             '&nbsp&nbsp&nbsp&nbsp' + // Non-breaking spaces to avoid clicking on the wrong icon
             '<a style="cursor:pointer;" class="revert-modification far fa-times-circle"></a></div>'
+}
+
+transactionAction(action, id){
+    let postURL = "../../api/log/?action=" + action + "&id=" + id;
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("POST", postURL, true);
+    xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhttp.onreadystatechange = function() {
+        if(xhttp.readyState === 4) {
+            if (xhttp.status !== 200) {
+                console.log(this);
+                new PNotify({
+                    title: 'Échec!',
+                    text: "L'action n'a pas pu être annulée.",
+                    type: 'error'
+                });
+            } else {
+                drawDataTable('logs');
+            }
+        }
+    };
+    xhttp.send();
 }
