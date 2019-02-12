0ed5350d683c66ffab035590f19eae3acbbb0b1f
adhallet <adrien.hallet@student.uclouvain.be>
2019-02-13 00:41:54 +0100

    Display a notification when a report is waiting

--- code/haitiwater/static-common/javascripts/theme.custom.js
+++ code/haitiwater/static-common/javascripts/theme.custom.js
@@ -1,3 +1,13 @@
+'use strict';
+let monthlyReportNotification = '' +
+    '<a href="../rapport" class="clearfix">' +
+        '<div class="image">' +
+        '</div>' +
+        '<span class="title">Rapport en attente</span>' +
+        '<span class="message">Un rapport est en attente,' +
+            'visitez la page des rapports pour l\'envoyer.</span>' +
+    '</a>';
+
 $( document ).ready(function() {
 
     //Get local storage value or true (as it is default to be open)
@@ -12,4 +22,45 @@ $( document ).ready(function() {
         localStorage.setItem('isMenuOpen', isMenuOpen.toString());
     })
 
+    setupNotifications();
 });
+
+function setupNotifications(){
+    let notificationParent = $('#notification-parent');
+    let notificationList = $('#notification-content');
+    let alertBadge = $('#alert-badge');
+    let classicBadge = $('#classic-badge')
+
+    let notificationCounter = 0;
+    if (notificationMonthlyReport(notificationList)) notificationCounter += 1;
+
+    classicBadge.html(notificationCounter);
+    alertBadge.html(notificationCounter);
+
+    if (notificationCounter > 0){
+        notificationParent.find('.badge').removeClass('hidden');
+    }
+    else {
+        notificationParent.find('.badge').addClass('hidden');
+    }
+
+}
+
+/**
+ * Adds a monthly report notification if one is waiting
+ * @return {boolean} true if a notification has been set, false otherwise
+ */
+function notificationMonthlyReport(notificationList){
+    console.log(localStorage.getItem('monthlyReport'));
+    let hasMonthlyReport = (localStorage.getItem('monthlyReport') !== null);
+    if (hasMonthlyReport){
+        appendNotification(notificationList, monthlyReportNotification);
+        return true;
+    }
+    return false;
+}
+
+function appendNotification(notificationList, notification){
+    let wrappedNotification = '<li>' + notification + '</li>';
+    notificationList.append(wrappedNotification);
+}
