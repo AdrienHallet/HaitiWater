1ca5536c94c1ed68f57646019c7b149a6320c985
CelineDknp <celine.deknop@student.uclouvain.be>
2019-02-17 00:26:43 +0100

    Further refactoring

--- code/haitiwater/apps/log/utils.py
+++ code/haitiwater/apps/log/utils.py
@@ -84,101 +84,119 @@ def re_add_item(logs):
 
 def restore_item(dict, table):
     if table == "Consumer":
-        outlet = Element.objects.filter(id=dict["Sortie d'eau"])
-        if len(outlet) != 1:
-            return HttpResponse("Impossible de restaurer cet élément", status=500)
-        outlet = outlet[0]
-        restored = Consumer(last_name=dict["Nom"], first_name=dict["Prénom"],
-                          gender=dict["Genre"], location=dict["Adresse"], phone_number=dict["Numéro de téléphone"],
-                          email="", household_size=dict["Taille du ménage"], water_outlet=outlet)
-        restored.save()
+        restore_consumer(dict)
     elif table == "Ticket":
-        outlet = Element.objects.filter(id=dict["Sortie d'eau concernée"])
-        if len(outlet) != 1:
-            return HttpResponse("Impossible de restaurer cet élément", status=500)
-        outlet = outlet[0]
-        restored = Ticket(water_outlet=outlet, type=dict["Type de panne"],
-                          comment=dict["Commentaire"], urgency=dict["Niveau d'urgence"],
-                          status=dict["Etat de résolution"], image=None)
-        restored.save()
+        restore_outlet(dict)
     elif table == "WaterElement":
-        zone = Zone.objects.filter(id=dict["Zone de l'élément"])
-        if len(zone) != 1:
-            return HttpResponse("Impossible de restaurer cet élément", status=500)
-        zone = zone[0]
-        restored = Element(name=dict["Nom"], type=dict["Type"],
-                           status=dict["État"], location=dict["Localisation"],
-                           zone=zone)
-        restored.save()
+        restore_water_element(dict)
     elif table == "Zone":
-        id_zone = dict["Zone mère"].split()[0]
-        super_zone = Zone.objects.filter(id=id_zone)
-        if len(super_zone) != 1:
-            return HttpResponse("Impossible de restaurer cet élément", status=500)
-        super_zone = super_zone[0]
-        restored = Zone(name=dict["Nom"], superzone=super_zone, subzones=[])
-        up = True
-        while up:
-            super_zone.subzones.append(dict["Nom"])
-            super_zone.save()
-            super_zone = super_zone.superzone
-            if super_zone == None:
-                up = False
-        restored.save()
+        restore_zone(dict)
     elif table == "Report":
         pass
     elif table == "User":
-        password = User.objects.make_random_password()  # New random password
-        user = User.objects.create_user(username=dict["Identifiant"],
-                                        email=dict["Email"],
-                                        password=password,
-                                        first_name=dict["Prénom"],
-                                        last_name=dict["Nom de famille"])
+        restore_user(dict)
 
-        if dict["Role"] == "Gestionnaire de fontaine":
-            import ast
-            water_out = ast.literal_eval(dict["outlets"])
-            if len(water_out) < 1:
-                user.delete()
-                return HttpResponse("Vous n'avez pas choisi de fontaine a attribuer !", status=500)
-            elif len(water_out) > 1:
-                res = Element.objects.filter(id__in=water_out)
-            else:
-                res = Element.objects.filter(id=water_out[0])
-            if len(res) > 0:
-                for outlet in res:
-                    user.profile.outlets.append(outlet.id)
-            else:
-                user.delete()
-                return HttpResponse("Impossible d'attribuer cette fontaine au gestionnaire", status=404)
-            my_group = Group.objects.get(name='Gestionnaire de fontaine')
-            my_group.user_set.add(user)
-        elif dict["Role"] == "Gestionnaire de zone":
-            zone = dict["Zone gérée"]
-            print("zone")
-            res = Zone.objects.filter(id=zone)
-            print(res)
-            if len(res) == 1:
-                user.profile.zone = res[0]
-                user.save()
-            else:
-                user.delete()
-                return HttpResponse("Impossible d'attribuer cette zone au gestionnaire", status=404)
-            my_group = Group.objects.get(name='Gestionnaire de zone')
-            my_group.user_set.add(user)
+
+def restore_consumer(dict):
+    outlet = Element.objects.filter(id=dict["Sortie d'eau"])
+    if len(outlet) != 1:
+        return HttpResponse("Impossible de restaurer cet élément", status=500)
+    outlet = outlet[0]
+    restored = Consumer(last_name=dict["Nom"], first_name=dict["Prénom"],
+                        gender=dict["Genre"], location=dict["Adresse"], phone_number=dict["Numéro de téléphone"],
+                        email="", household_size=dict["Taille du ménage"], water_outlet=outlet)
+    restored.save()
+
+
+def restore_outlet(dict):
+    outlet = Element.objects.filter(id=dict["Sortie d'eau concernée"])
+    if len(outlet) != 1:
+        return HttpResponse("Impossible de restaurer cet élément", status=500)
+    outlet = outlet[0]
+    restored = Ticket(water_outlet=outlet, type=dict["Type de panne"],
+                      comment=dict["Commentaire"], urgency=dict["Niveau d'urgence"],
+                      status=dict["Etat de résolution"], image=None)
+    restored.save()
+
+
+def restore_water_element(dict):
+    zone = Zone.objects.filter(id=dict["Zone de l'élément"])
+    if len(zone) != 1:
+        return HttpResponse("Impossible de restaurer cet élément", status=500)
+    zone = zone[0]
+    restored = Element(name=dict["Nom"], type=dict["Type"],
+                       status=dict["État"], location=dict["Localisation"],
+                       zone=zone)
+    restored.save()
+
+
+def restore_zone(dict):
+    id_zone = dict["Zone mère"].split()[0]
+    super_zone = Zone.objects.filter(id=id_zone)
+    if len(super_zone) != 1:
+        return HttpResponse("Impossible de restaurer cet élément", status=500)
+    super_zone = super_zone[0]
+    restored = Zone(name=dict["Nom"], superzone=super_zone, subzones=[])
+    up = True
+    while up:
+        super_zone.subzones.append(dict["Nom"])
+        super_zone.save()
+        super_zone = super_zone.superzone
+        if super_zone is None:
+            up = False
+    restored.save()
+
+
+def restore_user(dict):
+    password = User.objects.make_random_password()  # New random password
+    user = User.objects.create_user(username=dict["Identifiant"],
+                                    email=dict["Email"],
+                                    password=password,
+                                    first_name=dict["Prénom"],
+                                    last_name=dict["Nom de famille"])
+
+    if dict["Role"] == "Gestionnaire de fontaine":
+        import ast
+        water_out = ast.literal_eval(dict["outlets"])
+        if len(water_out) < 1:
+            user.delete()
+            return HttpResponse("Vous n'avez pas choisi de fontaine a attribuer !", status=500)
+        elif len(water_out) > 1:
+            res = Element.objects.filter(id__in=water_out)
+        else:
+            res = Element.objects.filter(id=water_out[0])
+        if len(res) > 0:
+            for outlet in res:
+                user.profile.outlets.append(outlet.id)
         else:
             user.delete()
-            return HttpResponse("Impossible d'ajouter l'utilisateur", status=500)
-        send_mail(
-            'Changement de mot de passe.',
-            'Votre compte haitiwater a été modifié, vous devez donc en changer le mot de passe.'+
-            '\nVoici votre nouveau mot de passe autogénéré : ' + password +
-            '\nVeuillez vous connecter pour le modifier.\nPour rappel, ' +
-            'votre identifiant est : ' + dict["Identifiant"],
-            '',
-            [dict["Email"]],
-            fail_silently=False,
-        )
+            return HttpResponse("Impossible d'attribuer cette fontaine au gestionnaire", status=404)
+        my_group = Group.objects.get(name='Gestionnaire de fontaine')
+        my_group.user_set.add(user)
+    elif dict["Role"] == "Gestionnaire de zone":
+        zone = dict["Zone gérée"]
+        res = Zone.objects.filter(id=zone)
+        if len(res) == 1:
+            user.profile.zone = res[0]
+            user.save()
+        else:
+            user.delete()
+            return HttpResponse("Impossible d'attribuer cette zone au gestionnaire", status=404)
+        my_group = Group.objects.get(name='Gestionnaire de zone')
+        my_group.user_set.add(user)
+    else:
+        user.delete()
+        return HttpResponse("Impossible d'ajouter l'utilisateur", status=500)
+    send_mail(
+        'Changement de mot de passe.',
+        'Votre compte haitiwater a été modifié, vous devez donc en changer le mot de passe.' +
+        '\nVoici votre nouveau mot de passe autogénéré : ' + password +
+        '\nVeuillez vous connecter pour le modifier.\nPour rappel, ' +
+        'votre identifiant est : ' + dict["Identifiant"],
+        '',
+        [dict["Email"]],
+        fail_silently=False,
+    )
 
 
 def get_elem_logged(logs):
