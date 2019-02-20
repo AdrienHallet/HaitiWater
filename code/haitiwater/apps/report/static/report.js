38bf0f858436c6e00e600f333bc360e6c1f8c8a6
Unknown <adrien.hallet@student.uclouvain.be>
2019-02-20 17:00:39 +0100

    Correct import order, still need to import it in header

--- code/haitiwater/apps/report/static/report.js
+++ code/haitiwater/apps/report/static/report.js
@@ -19,9 +19,10 @@ function showModal(id){
 }
 
 $(document).ready(function() {
-    if (location.pathname !== '/offline/')
+    if (location.pathname !== '/offline/') {
         drawTicketTable();
         drawReportTable();
+    }
 
     $('#input-picture').on('change', function(){
         readURL(this);
