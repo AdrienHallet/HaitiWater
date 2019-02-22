ecad4164b5ffb96efba4db4400938694bfc93188
adhallet <adrien.hallet@student.uclouvain.be>
2019-02-22 16:59:45 +0100

    Collapse without dedicated JS

--- code/haitiwater/apps/help/static/help.js
+++ code/haitiwater/apps/help/static/help.js
@@ -1,19 +1,3 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-    let coll = document.getElementsByClassName("collapsible");
-
-    for (let i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            console.log('click');
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight){
-                content.style.display = 'none';
-                content.style.maxHeight = null;
-            } else {
-                content.style.display = 'block';
-                content.style.maxHeight = content.scrollHeight + "px";
-            }
-        });
-    }
 });
