8a2e1f2c3841faec30b5e68e0389f5fe8f005c31
adhallet <adrien.hallet@student.uclouvain.be>
2019-03-11 18:26:37 +0100

    Help page advanced

--- code/haitiwater/apps/help/static/help.js
+++ code/haitiwater/apps/help/static/help.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    let triggerHelpAnimation = document.getElementById("trigger-help-animation");
+    triggerHelpAnimation.addEventListener("mouseover", animateHelpButton, false);
+    triggerHelpAnimation.addEventListener("mouseout", deanimateHelpButton, false);
 
 });
+
+function animateHelpButton(){
+    document.getElementById("quick-help").classList.add("breathing-color");
+}
+
+function deanimateHelpButton(){
+    document.getElementById("quick-help").classList.remove("breathing-color");
+}
+
