9a18f363f8cbd79c01fc1449880ab9d00bfada97
Sébastien Strebelle <sebastien.strebelle@student.uclouvain.be>
2019-03-18 11:24:28 +0100

    Merged Person into Consumer

--- code/haitiwater/apps/consumers/models.py
+++ code/haitiwater/apps/consumers/models.py
@@ -6,28 +6,22 @@ from ..utils.common_models import *
 from ..water_network.models import Location, Element
 
 
-class Person(models.Model):
+class Consumer(models.Model):
 
     first_name = models.CharField("Prénom", max_length=20)
     last_name = models.CharField("Nom", max_length=20)
-    gender = models.CharField("Genre", max_length=1, choices=[("M", "Homme"), ("F", "Femme"), ("O", "Autre")], null=True)
+    gender = models.CharField("Genre", max_length=1, choices=[("M", "Homme"), ("F", "Femme"), ("O", "Autre")],
+                              null=True)
     phone_number = models.CharField("Numéro de téléphone", max_length=10, null=True)
     email = models.CharField("Adresse email", max_length=50, null=True)
     location = models.CharField("Adresse", max_length=50)
-
-    def __str__(self):
-        return self.first_name + " " + self.last_name
-
-    class Meta:
-        abstract = True
-
-
-class Consumer(Person):
-
     household_size = models.IntegerField("Taille du ménage")
     water_outlet = models.ForeignKey(Element, verbose_name="Sortie d'eau", related_name="consumers", on_delete=models.CASCADE)
     creation_date = models.DateTimeField("Date de création", auto_now_add=True)
     #Consumer's zone is infered regarding the water_outlet he uses
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
     def descript(self):
         tab = [self.id, self.last_name, self.first_name, self.get_gender_display(),
