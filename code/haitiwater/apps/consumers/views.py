fca6c694226a68d5a5424956102161295ac806ba
adhallet <adrien.hallet@student.uclouvain.be>
2019-04-01 20:28:51 +0200

    Redirect to login when user is not authenticated on a restricted page

--- code/haitiwater/apps/consumers/views.py
+++ code/haitiwater/apps/consumers/views.py
@@ -2,8 +2,9 @@ from django.http import HttpResponse
 from django.template import loader
 from ..utils.get_data import *
 from haitiwater.settings import PROJECT_VERSION, PROJECT_NAME
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url='/login/')
 def index(request):
     template = loader.get_template('consumers.html')
     context = {
--- code/haitiwater/apps/financial/views.py
+++ code/haitiwater/apps/financial/views.py
@@ -2,8 +2,10 @@ from django.http import HttpResponse
 from django.template import loader
 from haitiwater.settings import PROJECT_VERSION, PROJECT_NAME
 from ..utils.get_data import get_zone, get_outlets
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='/login/')
 def index(request):
     template = loader.get_template('financial.html')
     context = {
